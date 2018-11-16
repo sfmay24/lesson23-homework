85f91c354bcd414238262fbfea866e43d86a3538
藤本將暉 <sfmay24@SFmay24.local>
2018-11-16 20:21:32 +0900

    syukudai

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 15 }
+  validates :email, presence: true, format: {
+    with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password,format: {
+    with: /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]{8,32}+\z/
+  }
+  has_secure_password
+end
---
+++ vendor/bundle/gems/ffi-1.9.25/ext/ffi_c/libffi-x86_64-darwin18/include/ffitarget.h
@@ -0,0 +1,1 @@
+/Users/sfmay24/pictgram/vendor/bundle/gems/ffi-1.9.25/ext/ffi_c/libffi/src/x86/ffitarget.h
